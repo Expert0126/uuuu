262f56373246d7b6183d1f50da49d46e5f0f077d
juliakater <juliakater1219@gmail.com>
2017-05-15 18:24:41 +0800

    project

---
+++ db.js
@@ -0,0 +1,14 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('hunchly.db');
+
+// db.serialize(function() {
+//     db.run("CREATE TABLE lorem (info TEXT)");
+//
+//       var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+//         for (var i = 0; i < 10; i++) {
+//                 stmt.run("Ipsum " + i);
+//                   }
+//           stmt.finalize();
+//
+//
+// });
